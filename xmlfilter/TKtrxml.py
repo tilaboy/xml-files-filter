1d945e1b3aa3da64541cbc24d2a80b27e54190a4
chao <chaoli@textkernel.nl>
2018-05-08 11:08:09 +0200

    wip

---
+++ xmlfilter/TKtrxml.py
@@ -0,0 +1,59 @@
+import sys
+import xml.etree.ElementTree as ET
+import os
+import re
+
+class TKtrxml():
+    def __init__(self, trxml_file=None):
+        self.trxml_file = trxml_file
+
+        self.size = os.path.getsize(trxml_file)
+        #print('create a new trxml object from {}'.format(self.trxml_file))
+
+        with open(trxml_file, 'r') as f:
+            trxml_string = f.read()
+            self.trxml_string = trxml_string
+            # print(trxml_string)
+
+        try:
+            tree = ET.parse(trxml_file)
+            root = tree.getroot()
+
+            self.topLevelTag = root.tag
+            self.ExtractionPart = root.find('Document')
+            self.DerivedPart = root.find('DocumentStructure')
+        except Exception as e:
+            print("ERROR: parse failed\n")
+            print(e)
+
+        try:
+            xpath = "ItemGroup[@key='{}']/Item[{}]/Field[@key='{}']/Value".format('address', '1', 'countrycode_nodefault')
+
+            self.country = self.DerivedPart.find(xpath).text
+
+        except Exception as e:
+            print("ERROR: country parsing failed\n")
+            print(e)
+
+
+        return
+
+    def get_account (self):
+        return self.ExtractionPart.get('account')
+
+    def get_lang(self):
+        return self.ExtractionPart.get('lang')
+
+    def get_size(self):
+        return int(self.size / 1024)
+
+    def get_orig_filetype(self):
+        mime_type = self.ExtractionPart.get('content_type')
+        return re.sub('^.*/', '' , mime_type)
+
+    def get_pathtype(self):
+        file_type = self.ExtractionPart.get('path', default='standard')
+        return file_type
+
+    def get_country(self):
+        return self.country
---
+++ xmlfilter/TKxml.py
@@ -0,0 +1,44 @@
+import sys
+import xml.etree.ElementTree as ET
+import os
+import re
+
+class TKxml():
+    def __init__(self, xml_file=None):
+        self.xml_file = xml_file
+
+        self.size = os.path.getsize(xml_file)
+        print('create a new xml object from {}'.format(self.xml_file))
+
+        with open(xml_file, 'r') as f:
+            xml_string = f.read()
+            self.xml_string = xml_string
+
+        try:
+            tree = ET.parse(xml_file)
+            root = tree.getroot()
+            print ('found root {}'.format(root.tag))
+            self.topLevelTag = root.tag
+            self.topLevelObj = root
+
+        except Exception as e:
+            print("ERROR: parse failed\n")
+            print(e)
+        return
+
+    def get_account (self):
+        return self.topLevelObj.get('account')
+
+    def get_lang(self):
+        return self.topLevelObj.get('lang')
+
+    def get_size(self):
+        return int(self.size / 1024)
+
+    def get_orig_filetype(self):
+        mime_type = self.topLevelObj.get('content_type')
+        return re.sub('^.*/', '' , mime_type)
+
+    def get_pathtype(self):
+        file_type = self.topLevelObj.get('path', default='standard')
+        return file_type
