7fb07059a883922534a44453337d111447f898b8
chao <chaoli@textkernel.nl>
2018-05-16 11:44:30 +0200

    return when file failed to validation

--- xml_filter.py
+++ xml_filter.py
@@ -12,30 +12,27 @@ from xml_filter.config import Config
 
 
 def validate_file(file_obj, config):
-    valid = 1
 
     attribute_filters = config.get('attribute_filters', {})
     for work_entity_tag, filters in attribute_filters.items():
         working_entity = file_obj.get_working_entity(work_entity_tag)
         if not validate_attributes(working_entity, filters):
-            valid = 0
-            break
+            return 0
 
     field_selectors = config.get('field_selectors', {})
     for work_entity_tag, selectors in field_selectors.items():
         working_entity = file_obj.get_working_entity(work_entity_tag)
         if not validate_fields(working_entity, selectors):
-            valid = 0
-            break
+            return 0
         
-        
-    return valid
+    return 1
 
 def validate_fields(working_entity, selectors):
     valid = 1
     for field_name, selector in selectors.items():
         retrieved = working_entity.find(selector.get('xpath')).text
-        #print("field {}: [{}] with [{}]".format(field_name, retrieved, selector.get('value')))
+        # print("field {}: [{}] with [{}]".format(field_name, retrieved, \
+        # selector.get('value')))
         if retrieved != selector.get('value'):
             valid = 0
             break
@@ -46,7 +43,8 @@ def validate_attributes(working_entity, filters):
     valid = 1
     for attribute_name, attribute_dic in filters.items():
         retrieved = get_attribute(working_entity, attribute_name, attribute_dic)
-        #print("attri {}: [{}] with [{}]".format(attribute_name, retrieved, attribute_dic.get('value')))
+        # print("attri {}: [{}] with [{}]".format(attribute_name, retrieved,\
+        # attribute_dic.get('value')))
         if not validate_attribute(retrieved, attribute_dic.get('value')):
             valid = 0
             break
@@ -54,10 +52,11 @@ def validate_attributes(working_entity, filters):
 
 def get_attribute (working_entity, attribute_name, attribute_dic):
     retrieved = None
+    default_ = None
     if attribute_dic.get('default'):
-        retrieved = working_entity.get(attribute_name, default=attribute_dic['default'])
-    else:
-        retrieved = working_entity.get(attribute_name)
+        default_  = attribute_dic.get('default')
+
+    retrieved = working_entity.get(attribute_name, default_)
     return retrieved
 
 def validate_attribute(retrieved, expected):
@@ -74,7 +73,8 @@ def validate_attribute(retrieved, expected):
 
 def get_args():
     parser = argparse.ArgumentParser(description='read the list of xml or trxml\
-    files, generate a list of documents which is a random subset selected using the specified requirements')
+    files, generate a list of documents which is a random subset selected using\
+    the specified requirements')
 
     parser.add_argument('--config', help='config file to config the selectors', type=str)
     parser.add_argument('--input_dir', help='dir contains all xml or trxml to filter',\
